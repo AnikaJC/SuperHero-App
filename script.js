eb912eee3be90fcc134b0732ddc3a064de221727
Anika Jahan Choudhury <68474664+AnikaJC@users.noreply.github.com>
2023-01-02 14:44:48 +0000

    now,it is dynamic!!

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-
-const getSuperHero =() => {
-  fetch('https://superheroapi.com/api.php/10223569763528853/346')
+const SUPERHERO_TOKEN = '10223569763528853'
+const BASE_URL = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`
+const getSuperHero =(id, name) => {
+  fetch(`${BASE_URL}/${id}`)
   .then(response => response.json())
-  .then(json => console.log(json.image.url))
+  .then(json => {
+    console.log(json)
+    document.querySelector('body').innerHTML += `<img src = "${json.image.url}" height = 250, width = 250/>`             
+  })
 }
-getSuperHero()
+getSuperHero(346)
+//const img = "https://www.superherodb.com/pictures2/portraits/10/100/85.jpg"
+
+
+
+
+//document.querySelector('body').innerHTML += 'goodbye'
